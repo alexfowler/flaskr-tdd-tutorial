25926b6b5f5fd794f4311a42b88e2fd0256b0d9a
afowler <afowler@moneylion.com>
2019-04-23 18:37:45 +0800

    Setup sqlite3 database

--- app-test.py
+++ app-test.py
@@ -1,3 +1,5 @@
+import os
+
 from app import app
 
 import unittest
@@ -8,8 +10,11 @@ class BasicTestCast(unittest.TestCase):
     def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'Hello, World!')
+        self.assertEqual(response.status_code, 404)
+
+    def test_database(self):
+        tester = os.path.exists('flaskr.db')
+        self.assertTrue(tester)
 
 
 if __name__ == '__main__':
